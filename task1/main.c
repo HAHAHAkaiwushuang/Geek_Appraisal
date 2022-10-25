ea909469bf4069bad81537a23a315f1f3dc95ffe
HAHAHAkaiwushuang <3335145920@qq.com>
2022-10-26 00:32:25 +0800

    Modify main.c

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+int main() 
+{
+	double y=0;
+	double a,b,c,x;
+	printf("a=");
+	scanf("%lf",&a);
+	printf("    x             y\n");
+	b=a;
+	c=b+1.0;
+	for(x=0.001;c-b>0.000001;x+=0.001)
+	{
+		y=0.999*b;
+		c=b;
+		b=y;
+		printf("%f      %f\n",x,y);
+	}
 	return 0;
 }
 
